b41333ddccb2756d74783423c757428cea4e1c91
Ravivarma <ravi.varma.madasu.2@gmail.com>
2024-04-17 00:33:39 +0530

    libraries, permissions defined

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //882289c88dd74c3b86d1b7825fda18ba	---Master API key
     }
 }
